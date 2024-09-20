1f88bf9869f1d82372f9af835448466248c918a9
Christian-Nils Boda <christian-nils.boda@autoliv.com>
2024-09-20 23:00:43 +0200

    feat&fix: change first free memory address and add LED blink

--- PMRA_B2/main.c
+++ PMRA_B2/main.c
@@ -18,21 +18,25 @@
 unsigned long my_thread_counter = 0;
 TX_THREAD my_thread;
 
-int main()
-{
-  /* Enter the ThreadX kernel. */
-  tx_kernel_enter();
-}
-
 void my_thread_entry(ULONG thread_input)
 {
+  rcu_periph_clock_enable(RCU_GPIOF);
+  gpio_init(LED_STATUS_PORT, GPIO_MODE_OUT_PP, GPIO_OSPEED_50MHZ, LED_STATUS_PIN);
   /* Enter into a forever loop. */
   while (1)
   {
     /* Increment thread counter. */
-    my_thread_counter++;
+    my_thread_counter = !my_thread_counter;
+    if (my_thread_counter == 0)
+    {
+      gpio_bit_reset(LED_STATUS_PORT, LED_STATUS_PIN);
+    }
+    else
+    {
+      gpio_bit_set(LED_STATUS_PORT, LED_STATUS_PIN);
+    }
     /* Sleep for 1 tick. */
-    tx_thread_sleep(1);
+    tx_thread_sleep(1000);
   }
 }
 
@@ -42,4 +46,11 @@ void tx_application_define(void *first_unused_memory)
   tx_thread_create(&my_thread, "My Thread",
                    my_thread_entry, 0x1234, first_unused_memory, 1024,
                    3, 3, TX_NO_TIME_SLICE, TX_AUTO_START);
+}
+
+int main()
+{
+  /* Enter the ThreadX kernel. */
+  tx_kernel_enter();
+  return 0;
 }
